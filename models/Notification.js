5f51c3dbf8f8741de22a676251ef8f53ede1f5b1
MR_DIV <vshntvelip@gmail.com>
2026-08-17 20:52:41 +0530

    Add direct message notifications

--- models/Notification.js
+++ models/Notification.js
@@ -1,19 +1,5 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
-
-const NotificationSchema = new Schema({
-    recipient: { type: Schema.Types.ObjectId, ref: "user", required: true },
-    type: { type: String, enum: ["comment", "reply", "like", "follow", "follow_request", "mention", "blog_post"], required: true },
-    title: String,
-    message: String,
-    blog: { type: Schema.Types.ObjectId, ref: "blog" },
-    actor: { type: Schema.Types.ObjectId, ref: "user" },
-    request: { type: Schema.Types.ObjectId, ref: "FollowRequest" },
-    isRead: { type: Boolean, default: false }
-}, { timestamps: true });
-
-NotificationSchema.index({ recipient: 1, isRead: 1 });
-NotificationSchema.index({ createdAt: -1 });
-
-const Notification = mongoose.models.Notification || model("Notification", NotificationSchema);
-module.exports = Notification;
+const NotificationSchema = new Schema({recipient:{type:Schema.Types.ObjectId,ref:"user",required:true},type:{type:String,enum:["comment","reply","like","follow","follow_request","mention","blog_post","message"],required:true},title:String,message:String,blog:{type:Schema.Types.ObjectId,ref:"blog"},actor:{type:Schema.Types.ObjectId,ref:"user"},request:{type:Schema.Types.ObjectId,ref:"FollowRequest"},isRead:{type:Boolean,default:false}},{timestamps:true});
+NotificationSchema.index({recipient:1,isRead:1});NotificationSchema.index({createdAt:-1});
+const Notification=mongoose.models.Notification||model("Notification",NotificationSchema);module.exports=Notification;
