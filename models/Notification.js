dd8524f0a799daeb79c7e792f4e06f83b51a32ee
MR_DIV <vshntvelip@gmail.com>
2026-08-17 18:04:44 +0530

    Support private-account follow request notifications

--- models/Notification.js
+++ models/Notification.js
@@ -2,38 +2,16 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const NotificationSchema = new Schema({
-    recipient: {
-        type: Schema.Types.ObjectId,
-        ref: "user",
-        required: true
-    },
-    
-    type: {
-        type: String,
-        enum: ["comment", "reply", "like", "follow", "mention", "blog_post"],
-        required: true
-    },
-    
+    recipient: { type: Schema.Types.ObjectId, ref: "user", required: true },
+    type: { type: String, enum: ["comment", "reply", "like", "follow", "follow_request", "mention", "blog_post"], required: true },
     title: String,
     message: String,
-    
-    // Related entities
-    blog: {
-        type: Schema.Types.ObjectId,
-        ref: "blog"
-    },
-    
-    actor: {
-        type: Schema.Types.ObjectId,
-        ref: "user"
-    },
-    
-    // Status
-    isRead: { type: Boolean, default: false },
-    
+    blog: { type: Schema.Types.ObjectId, ref: "blog" },
+    actor: { type: Schema.Types.ObjectId, ref: "user" },
+    request: { type: Schema.Types.ObjectId, ref: "FollowRequest" },
+    isRead: { type: Boolean, default: false }
 }, { timestamps: true });
 
-// Index for performance
 NotificationSchema.index({ recipient: 1, isRead: 1 });
 NotificationSchema.index({ createdAt: -1 });
 
