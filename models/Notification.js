4bec6e45a9d481e0b3465dc81197c873e8043d22
MR_DIV <vshntvelip@gmail.com>
2026-08-17 22:03:11 +0530

    Store message reference in notifications

--- models/Notification.js
+++ models/Notification.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const { Schema, model } = mongoose;
-const NotificationSchema = new Schema({recipient:{type:Schema.Types.ObjectId,ref:"user",required:true},type:{type:String,enum:["comment","reply","like","follow","follow_request","mention","blog_post","message"],required:true},title:String,message:String,blog:{type:Schema.Types.ObjectId,ref:"blog"},actor:{type:Schema.Types.ObjectId,ref:"user"},request:{type:Schema.Types.ObjectId,ref:"FollowRequest"},isRead:{type:Boolean,default:false}},{timestamps:true});
-NotificationSchema.index({recipient:1,isRead:1});NotificationSchema.index({createdAt:-1});
+const mongoose=require("mongoose");
+const {Schema,model}=mongoose;
+const NotificationSchema=new Schema({recipient:{type:Schema.Types.ObjectId,ref:"user",required:true},type:{type:String,enum:["comment","reply","like","follow","follow_request","mention","blog_post","message"],required:true},title:String,message:String,blog:{type:Schema.Types.ObjectId,ref:"blog"},actor:{type:Schema.Types.ObjectId,ref:"user"},request:{type:Schema.Types.ObjectId,ref:"FollowRequest"},messageRef:{type:Schema.Types.ObjectId,ref:"Message",default:null},conversationId:{type:Schema.Types.ObjectId,ref:"Conversation",default:null},isRead:{type:Boolean,default:false}},{timestamps:true});
+NotificationSchema.index({recipient:1,isRead:1});NotificationSchema.index({createdAt:-1});NotificationSchema.index({recipient:1,type:1,messageRef:1});
 const Notification=mongoose.models.Notification||model("Notification",NotificationSchema);module.exports=Notification;
