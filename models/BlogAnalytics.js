bf225a177311b5da026cd58d099acded184c5357
MR_DIV <vshntvelip@gmail.com>
2026-08-18 19:43:54 +0530

    Expand BlogAnalytics for traffic sources and performance indexes

--- models/BlogAnalytics.js
+++ models/BlogAnalytics.js
@@ -2,55 +2,34 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const BlogAnalyticsSchema = new Schema({
-    blog: {
-        type: Schema.Types.ObjectId,
-        ref: "blog",
-        required: true
-    },
-    
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "user",
-        required: true
-    },
-    
+    blog: { type: Schema.Types.ObjectId, ref: "blog", required: true },
+    author: { type: Schema.Types.ObjectId, ref: "user", required: true },
     totalViews: { type: Number, default: 0 },
     totalLikes: { type: Number, default: 0 },
     totalComments: { type: Number, default: 0 },
     totalShares: { type: Number, default: 0 },
-    
-    // Daily stats
-    dailyViews: [{
-        date: Date,
-        count: Number
-    }],
-    
-    // Source tracking
+    dailyViews: [{ date: Date, count: Number }],
     viewSource: {
         direct: { type: Number, default: 0 },
         search: { type: Number, default: 0 },
         social: { type: Number, default: 0 },
-        referral: { type: Number, default: 0 }
+        referral: { type: Number, default: 0 },
+        home: { type: Number, default: 0 },
+        profile: { type: Number, default: 0 },
+        notification: { type: Number, default: 0 }
     },
-    
-    // Device tracking
     deviceStats: {
         mobile: { type: Number, default: 0 },
         tablet: { type: Number, default: 0 },
         desktop: { type: Number, default: 0 }
     },
-    
-    // Geography
-    topCountries: [{
-        country: String,
-        views: Number
-    }],
-    
+    topCountries: [{ country: String, views: Number }]
 }, { timestamps: true });
 
-BlogAnalyticsSchema.index({ blog: 1 });
+BlogAnalyticsSchema.index({ blog: 1 }, { unique: true });
 BlogAnalyticsSchema.index({ author: 1 });
+BlogAnalyticsSchema.index({ totalViews: -1 });
+BlogAnalyticsSchema.index({ totalLikes: -1 });
 
 const BlogAnalytics = mongoose.models.BlogAnalytics || model("BlogAnalytics", BlogAnalyticsSchema);
 module.exports = BlogAnalytics;
-
