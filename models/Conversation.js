9987e5499aa71efba981ca803246223ba3c95298
MR_DIV <vshntvelip@gmail.com>
2026-08-22 15:33:24 +0530

    Add per-user conversation archive support

--- models/Conversation.js
+++ models/Conversation.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
-const conversationSchema=new mongoose.Schema({participants:[{type:mongoose.Schema.Types.ObjectId,ref:"user",required:true}],lastMessage:{type:mongoose.Schema.Types.ObjectId,ref:"Message",default:null},lastMessageAt:{type:Date,default:null},blockedBy:[{type:mongoose.Schema.Types.ObjectId,ref:"user"}],mutedBy:[{type:mongoose.Schema.Types.ObjectId,ref:"user"}]},{timestamps:true});
+const conversationSchema=new mongoose.Schema({participants:[{type:mongoose.Schema.Types.ObjectId,ref:"user",required:true}],lastMessage:{type:mongoose.Schema.Types.ObjectId,ref:"Message",default:null},lastMessageAt:{type:Date,default:null},blockedBy:[{type:mongoose.Schema.Types.ObjectId,ref:"user"}],mutedBy:[{type:mongoose.Schema.Types.ObjectId,ref:"user"}],archivedBy:[{type:mongoose.Schema.Types.ObjectId,ref:"user",index:true}]},{timestamps:true});
 conversationSchema.index({participants:1});
+conversationSchema.index({participants:1,archivedBy:1});
 module.exports=mongoose.model("Conversation",conversationSchema);
